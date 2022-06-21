db13e3e4417cc6ec88b51d1023c715a1a99d3098
karine <karine.machado@commbox.com.br>
2022-06-21 08:54:47 -0300

    Implementação da página Home e seus componentes

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -3,23 +3,34 @@ import "./menu.scss";
 import logo from "../../assents/logo.png"
 import imgYellow from "../../assents/yellow.png"
 
-
+const menuItems = [{
+    name: "Como fazer",
+    link: ""
+}, {
+    name: "Ofertas",
+    link: ""
+}, {
+    name: "Depoimentos",
+    link: ""
+}, {
+    name: "Videos",
+    link: ""
+}, {
+    name: "Meu carrinho",
+    link: ""
+}]
 
 export default function Menu(): JSX.Element {
+
     return (
         <div className="menu">
             <img className="image-menu" src={logo} />
             <img className="image-yellow" src={imgYellow} />
             <ul className="navegation-menu">
-                <li className="navegation-item">Como fazer</li>
-                <li style={{ listStyle: 'none' }}>/</li>
-                <li className="navegation-item">Ofertas</li>
-                <li style={{ listStyle: 'none' }} >/</li>
-                <li className="navegation-item">Depoimentos</li>
-                <li style={{ listStyle: 'none' }} >/</li>
-                <li className="navegation-item">Videos</li>
-                <li style={{ listStyle: 'none' }} >/</li>
-                <li className="navegation-item">Meu carrinho</li>
+                {menuItems.map((item, i, array) => <React.Fragment key={i}>
+                    {i === 0 ? null : <li style={{ listStyle: 'none' }}>/</li>}
+                    <li className="navegation-item">{item.name}</li>
+                </React.Fragment>)}
             </ul>
         </div>
     )
